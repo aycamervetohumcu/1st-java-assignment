07d259406d252d064c205f41c5fc0f0f69f8a090
aycamervetohumcu <amucar06@gmail.com>
2024-10-15 00:06:14 +0200

    name is changed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let fname;
 let age;
 let colour;
 
-fname = "Ayca Merve Tohumcu"; 
+fname = "Ayca Merve"; 
 age = 36;
 colour = "blue and green";
 console.log(`My name is ${fname}, I am ${age} years old, and my favourite colours are ${colour}.`);
